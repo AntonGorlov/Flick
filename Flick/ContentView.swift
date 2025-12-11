b68e342548955e14268639800946cbcfb3daba68
Anton Gorlov <ta7oshka@gmail.com>
2025-12-11 12:50:06 +0200

    I've added gitignore

---
+++ Flick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flick
+//
+//  Created by Anton Gorlov on 11.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flick/FlickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickApp.swift
+//  Flick
+//
+//  Created by Anton Gorlov on 11.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
